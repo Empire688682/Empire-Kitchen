10e46f096631bfd85a08f5ce45da5646ab557737
Empire688682 <asehindej@gmail.com>
2024-08-19 15:43:14 +0100

    profile

--- FrontEnd/src/Component/Profile/Profile.jsx
+++ FrontEnd/src/Component/Profile/Profile.jsx
@@ -2,9 +2,13 @@ import './Profile.css';
 import React, { useState } from 'react';
 import axios from 'axios';
 import { UseGlobalContext } from '../../Context';
+import { FaUser } from "react-icons/fa";
+import { BsFillBagFill } from "react-icons/bs";
+import { IoLogOut } from "react-icons/io5";
+import { useNavigate} from 'react-router-dom';
 
 const Profile = () => {
-  const {url} = UseGlobalContext()
+  const { url, setToken } = UseGlobalContext()
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("User Data")) || "");
   const [data, setData] = useState({
     userId: `${user._id}`,
@@ -16,13 +20,15 @@ const Profile = () => {
   });
   const [resMessage, setResMessage] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const [dashboard, setDashboard] = useState("information");
 
-  const edditUser = async () =>{
+  const edditUser = async () => {
     try {
       setLoading(true)
-      const response = await axios.post(url+"api/users/eddit", data);
-      if(response){
-        setTimeout(()=>{
+      const response = await axios.post(url + "api/users/eddit", data);
+      if (response) {
+        setTimeout(() => {
           setResMessage(false);
         }, 1000);
         setResMessage(true);
@@ -32,7 +38,7 @@ const Profile = () => {
     } catch (error) {
       console.log(error)
     }
-    finally{
+    finally {
       setLoading(false)
     }
   }
@@ -46,66 +52,86 @@ const Profile = () => {
     e.preventDefault();
     edditUser()
   };
-  
-  
+
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("cartItems");
+    setToken("");
+    navigate("/");
+    localStorage.removeItem("User Data")
+    window.location.reload();
+  }
+
+
   return (
     <div className='profile'>
       <div className="small_col">
-        <h2>ACCOUT DASHBOARD</h2>
+        <h2>ACCOUNT DASHBOARD</h2>
         <ul>
-          <li><span>Account Information</span></li>
-          <li><span>Address Book</span></li>
-          <li><span>My Orders</span></li>
+          <li onClick={() => setDashboard("information")}> <FaUser /><span>Account Information</span></li>
+          <li onClick={() => setDashboard("order")}><BsFillBagFill /><span>My Orders</span></li>
         </ul>
+        <p onClick={logoutUser}><IoLogOut /><span>Logout</span></p>
       </div>
-      <div className="big_col">
-        <div className="content_block_title">
-          <h2>User Information</h2>
-        </div>
-        <div className="info_form">
-        <form onSubmit={handleFormSubmission}>
-            <input
-              onChange={handleOnchange}
-              value={data.fName}
-              name="fName"
-              type="text"
-              placeholder='First Name'
-              required
-            />
-            <input
-              onChange={handleOnchange}
-              value={data.lName}
-              name="lName"
-              type="text"
-              placeholder='Last Name'
-              required
-            />
-          <input
-            onChange={handleOnchange}
-            value={data.email}
-            name="email"
-            type="email"
-            placeholder='Email'
-            required
-          />
-            <select onChange={handleOnchange} name="gender" value={data.gender}>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-            <input
-              onChange={handleOnchange}
-              value={data.dBirth}
-              name="dBirth"
-              type="date"
-              required
-            />
-            {
-              resMessage ? <p style={{color:"black"}}>Saved Changed</p>:""
-            }
-          <input type='submit' value={loading? "Saving...":"Save Change"}></input>
-        </form>
-        </div>
-      </div>
+      {
+        dashboard && dashboard === "information" ?
+          <div className="big_col">
+            <div className="content_block_title">
+              <h2>User Information</h2>
+            </div>
+            <div className="info_form">
+              <form onSubmit={handleFormSubmission}>
+                <input
+                  onChange={handleOnchange}
+                  value={data.fName}
+                  name="fName"
+                  type="text"
+                  placeholder='First Name'
+                  required
+                />
+                <input
+                  onChange={handleOnchange}
+                  value={data.lName}
+                  name="lName"
+                  type="text"
+                  placeholder='Last Name'
+                  required
+                />
+                <input
+                  onChange={handleOnchange}
+                  value={data.email}
+                  name="email"
+                  type="email"
+                  placeholder='Email'
+                  required
+                />
+                <select onChange={handleOnchange} name="gender" value={data.gender}>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+                <input
+                  onChange={handleOnchange}
+                  value={data.dBirth}
+                  name="dBirth"
+                  type="date"
+                  required
+                />
+                {
+                  resMessage ? <p style={{ color: "black" }}>Saved Changed</p> : ""
+                }
+                <input type='submit' value={loading ? "Saving..." : "Save Change"}></input>
+              </form>
+            </div>
+          </div>
+          :
+          <div className="big_col">
+            <div className="content_block_title">
+              <h2>My Order</h2>
+            </div>
+            <h3 style={{color:"black"}}>Comming Soon</h3>
+            <p style={{color:"black"}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus velit harum labore minima magnam, quod atque! Facilis sint, dolorum, architecto, ratione reiciendis esse ab at iure omnis est veritatis similique?</p>
+          </div>
+      }
     </div>
   )
 }
